bf9ba472cf6b9d4bb83fa0e2ce03968dcd2f00c3
Nicholas Egner <nick@nicholasegner.com>
2026-09-16 19:41:29 -0500

    commit

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>
-            To get started, edit the{" "}
+            TEST
             <code className={styles.code}>page.js</code> file.
           </h1>
           <p>
